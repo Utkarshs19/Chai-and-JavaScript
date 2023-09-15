175ecbeee7e46a5ee379e3049b31a77c6dbbc33b
Utkarsh <Utkarsh.sharma19072002@gmail.com>
2023-09-15 10:22:29 +0530

    Let Const Var

---
+++ index.js
@@ -0,0 +1,9 @@
+const accountID=20202
+let accountName="Utkarsh"
+var accountType="Saving"
+
+accountDate="10-10-2023"
+
+console.table([accountID,accountName,accountType,accountDate]);
+
+console.log(accountType)
